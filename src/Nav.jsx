80b21789b2cc204ca19dcd565cf354f6d375dbe4
RyanBulluss <rbulluss2000@gmail.com>
2024-12-07 23:08:17 +0000

    add new button

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,6 +5,7 @@ const Nav = ({ setFinding, resetState }) => {
 
     return (
         <nav className="w-full bg-slate-300 h-[10vh] flex justify-around">
+            <button className="hover:text-gray-500" onClick={resetState}>Change</button>
             <button className="hover:text-gray-500" onClick={resetState}>Clear</button>
             <button className="hover:text-gray-500" onClick={() => setFinding(f => !f)}>Start</button>
         </nav>
